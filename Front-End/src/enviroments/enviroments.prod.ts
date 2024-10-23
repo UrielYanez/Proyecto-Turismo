b32cedcc5f801a94a4b443ce315bb3ca5bf5f169
Vir-gil-io <164133792+Vir-gil-io@users.noreply.github.com>
2024-10-23 09:17:20 -0600

    gg

--- Front-End/src/enviroments/enviroments.prod.ts
+++ Front-End/src/enviroments/enviroments.prod.ts
@@ -1,4 +1,4 @@
 export const enviroments={
-    baseUrl: 'https://backend-production-70c9.up.railway.app'
-    // baseUrl: 'http://localhost:3000'
+    //baseUrl: 'https://backend-production-70c9.up.railway.app'
+    baseUrl: 'http://localhost:3000'
 }
