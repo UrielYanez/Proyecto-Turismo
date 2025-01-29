0951f8a1ef263ea06acd9f8ee35320ad7ed030d2
UrielYanez <aguayomar77@gmail.com>
2025-01-29 09:32:39 -0600

    Guards Activos

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-//import { LoginGuard } from './guards/login.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )},
-  { path: 'administrador', loadChildren: () => import('./administrador/administrador.module').then( m => m.AdministradorModule ), /*canActivate: [ LoginGuard ]*/},
+  { path: 'administrador', loadChildren: () => import('./administrador/administrador.module').then( m => m.AdministradorModule ), canActivate: [ LoginGuard ]},
   { path: '404', component: Error404PageComponent,},
   { path: '', redirectTo: 'auth', pathMatch: 'full'},
   { path: '**', redirectTo: '404'}
--- Front-End/src/app/guards/login.guard.ts
+++ Front-End/src/app/guards/login.guard.ts
@@ -1,4 +1,4 @@
-/*import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import { catchError, map, Observable, of } from 'rxjs';
 import { AuthServices } from '../auth/services/auth.service';
@@ -20,8 +20,8 @@ export class LoginGuard implements CanActivate {
       return true;
     } else {
       console.log('Token no válido o no presente');
-      this.router.navigate(['/login']);
+      this.router.navigate(['/auth/login']);
       return false;
     }
   }
-}*/
+}
